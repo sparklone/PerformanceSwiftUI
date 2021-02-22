c9ed82eef172442204b7956d7132aa1b3c08b410
Aleksandr Glushchenko <sparklone@gmail.com>
2021-02-22 18:54:42 +0300

    Initial Commit

---
+++ PerformanceSwiftUI/PerformanceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerformanceSwiftUIApp.swift
+//  PerformanceSwiftUI
+//
+//  Created by Alexandr Glushchenko on 22.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct PerformanceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
